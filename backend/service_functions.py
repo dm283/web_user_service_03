b7287ca34698ddf75c4df952f15f87f0a8d4e8b2
dm283 <dm283@yandex.ru>
2025-05-02 17:31:34 +0400

    carpass for printing 2 pages done

--- backend/service_functions.py
+++ backend/service_functions.py
@@ -4,12 +4,23 @@ from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfbase import pdfmetrics 
 from reportlab.lib import colors
 from reportlab.platypus import Table, TableStyle
-
 import qrcode
+import PIL
 
 
 def create_document_carpass(carpass, filepath, filename):
     # creates carpass pdf file for printing
+
+    # QR code creating
+    qrcode_data = f'GUID: {carpass.uuid}\n' \
+        f'Регистрационный № ТС: {carpass.ncar}\n' \
+        f'ФИО водителя: {carpass.driver_fio}\n' \
+        f'Телефон водителя: {carpass.driver_phone}\n'
+    qr = qrcode.QRCode(box_size=5)
+    qr.add_data(qrcode_data)
+    qr.make()
+    qr_code_image = qr.make_image()
+
     pdf = canvas.Canvas(filepath) 
     pdf.setTitle(filename) 
     pdfmetrics.registerFont(TTFont('Arial', 'verdana.ttf')) 
@@ -61,7 +72,6 @@ def create_document_carpass(carpass, filepath, filename):
     pdf.rect(195, 492, 12, 12); pdf.rect(247, 492, 12, 12)
     pdf.rect(195, 472, 12, 12); pdf.rect(247, 472, 12, 12)
 
-    # pdf.line(30, 650, 100, 650)
     pdf.drawString(30, 652, '__________________________________________')
 
     # ======================================================================  ЗАЯВЛЕНИЕ
@@ -116,86 +126,66 @@ def create_document_carpass(carpass, filepath, filename):
     f.wrapOn(pdf, width, height); f.drawOn(pdf, x, y)
 
 
-    # pdf.drawInlineImage(image, 155, 200) #x.y
+    pdf.setFont("Arial", 25); pdf.drawString(25, 300, 'НОМЕР СТОЯНКИ')
+    pdf.setFont("Arial", 120); pdf.drawString(25, 150, F'{carpass.place_n}')
+    pdf.drawInlineImage(qr_code_image, 265, 50) #x.y QR-code image
+
+    pdf.showPage()
+    #################################### PAGE 2
+
+    pdf.setFont("Arial", 7); pdf.drawString(20, 820, 'Я, водитель а/м')
+    pdf.setFont("Arial", 9); pdf.drawString(100, 820, f'{carpass.driver_fio}')
+    text_lines = [
+        '-проинформирован о месте уведомления таможенного органа о прибытии товаров и транспортных средств в место доставки.',
+        '-предупрежден о том, что я обязан уведомить таможенный орган о прибытии в рабочее время не позднее 3-х.',
+        'часов с момента прибытия, в нерабочее время - не позднее 3-х часов после начала работы таможенного поста.',
+        '-ознакомлен с правилами поведения в зоне таможенного контроля и обязуюсь их выполнять.',
+        '-знаю, что на территории ЗТК запрещено :',
+        '',
+        '- Приводить, привозить посторонних лиц.',
+        '- Провозить, проносить и распивать спиртные напитки.',
+        '- Производить ремонт, мойку автомобилей.',
+        '- Создавать конфликтную ситуацию с персоналом и другими водителями.',
+        '- Разбрасывать на территории мусор, остатки продуктов, кормить животных.',
+        '- Пользоваться открытым огнем (готовить пищу, использовать газовые балоны, паяльные лампы).',
+        '- Курение.',
+        '- Самовольно перемещать а/т по ЗТК/СВХ ООО "МТС".',
+        '- Справлять нужду в неположенном месте',
+        '',
+        'Нарушение правил СВХ влечет наложение административной ответственности в соответствии с законодательством РФ.',
+        '',
+        'С правилами внутреннего распорядка ознакомлен',
+    ]
+    pdf.setFont("Arial", 7)
+    text_y = 810
+    for text_line in text_lines:
+        pdf.drawString(20, text_y, text_line)
+        text_y -= 10
+    pdf.setFont("Arial", 7); pdf.drawString(20, 610, 'Водитель ____________________________ ')
+    pdf.setFont("Arial", 7); pdf.drawString(80, 603, 'подпись')
+    pdf.setFont("Arial", 7); pdf.drawString(20, 580, 'Претензий не имею')
+    pdf.setFont("Arial", 9); pdf.drawString(130, 580, f'{carpass.driver_fio}')
+    pdf.setFont("Arial", 7); pdf.drawString(300, 580, 'подпись водителя/экспедитора')
+    pdf.drawString(130, 577, '_________________________________________________________________')
+    pdf.setFont("Arial", 7); pdf.drawString(20, 560, 'Подтверждение получил')
+    pdf.setFont("Arial", 9); pdf.drawString(130, 560, f'{carpass.driver_fio}')
+    pdf.setFont("Arial", 7); pdf.drawString(300, 560, 'подпись водителя/экспедитора')
+    pdf.drawString(130, 557, '_________________________________________________________________')
+    pdf.drawString(20, 540, 'Дата, время выдачи документов')
+    pdf.drawString(20, 537, '__________________________________________________________')
+    pdf.drawString(300, 540, 'Дата, время выезда ТС')
+    pdf.drawString(300, 537, '__________________________________________________________')
+    pdf.drawString(20, 520, 'Номер пломбы')
+    pdf.drawString(20, 517, '__________________________________________________________')
+    pdf.drawString(300, 520, 'Подпись охраны')
+    pdf.drawString(300, 517, '__________________________________________________________')
+    pdf.drawString(20, 500, 'Подпись ответ. лица СВХ')
+    pdf.drawString(20, 497, '__________________________________________________________')
+    
+    pdf.line(30, 477, 560, 477)
+
+    parking_map_image = PIL.Image.open('saved_files/parking_map.jpg')
+    pdf.drawInlineImage(parking_map_image, 20, 25, 550, 440) #x.y parking map
+    
+
     pdf.save()
-
-
-
-
-
-
-# def odl_create_document_carpass(carpass, filepath, filename):
-#     # creates carpass pdf file for printing
-#     title = 'Пропуск ТС на въезд'
-#     subTitle = f'Рег. № ТС:  {carpass.ncar}'
-#     textLines = [ 
-#         f'Перевозчик:  {carpass.driver}', 
-#         f'ФИО водителя:  {carpass.driver_fio}', 
-#         f'Телефон водителя:  {carpass.driver_phone}', 
-#         f'Номер стоянки:  {carpass.place_n}', 
-#     ] 
-
-#     # QR code creating
-#     qrcode_data = f'GUID: {carpass.uuid}\n' \
-#         f'Регистрационный № ТС: {carpass.ncar}\n' \
-#         f'ФИО водителя: {carpass.driver_fio}\n' \
-#         f'Телефон водителя: {carpass.driver_phone}\n'
-#     qr = qrcode.QRCode(box_size=5)
-#     qr.add_data(qrcode_data)
-#     qr.make()
-#     image = qr.make_image()
-
-#     pdf = canvas.Canvas(filepath) 
-#     pdf.setTitle(filename) 
-#     pdfmetrics.registerFont(TTFont('Arial', 'verdana.ttf')) 
-#     pdf.setFont('Arial', 30) 
-#     pdf.drawCentredString(300, 770, title) 
-#     # pdf.setFillColorRGB(0, 0, 255) 
-#     pdf.setFont("Arial", 24) 
-#     pdf.drawCentredString(290, 720, subTitle) 
-#     pdf.line(30, 710, 550, 710) 
-#     text = pdf.beginText(40, 680) 
-#     text.setFont("Arial", 18) 
-#     for line in textLines: 
-#         text.textLine(line) 
-#     pdf.drawText(text) 
-#     pdf.drawInlineImage(image, 155, 200) #x.y
-#     pdf.save()
-
-
-# def create_document_exitcarpass(carpass, filepath, filename):
-#     # creates exitcarpass pdf file for printing
-#     title = 'Пропуск ТС на выезд'
-#     subTitle = f'Рег. № ТС:  {carpass.ncar}'
-#     textLines = [ 
-#         f'ФИО водителя:  {carpass.drv_man}', 
-#         f'Телефон водителя:  {carpass.dev_phone}', 
-#         f'№ документа выпуска:  {carpass.ndexit}', 
-#     ] 
-
-#     # QR code creating
-#     qrcode_data = f'GUID: {carpass.uuid}\n' \
-#         f'Регистрационный № ТС: {carpass.ncar}\n' \
-#         f'ФИО водителя: {carpass.drv_man}\n' \
-#         f'Телефон водителя: {carpass.dev_phone}\n'
-#     qr = qrcode.QRCode(box_size=5)
-#     qr.add_data(qrcode_data)
-#     qr.make()
-#     image = qr.make_image()
-
-#     pdf = canvas.Canvas(filepath) 
-#     pdf.setTitle(filename) 
-#     pdfmetrics.registerFont(TTFont('Arial', 'verdana.ttf')) 
-#     pdf.setFont('Arial', 30) 
-#     pdf.drawCentredString(300, 770, title) 
-#     # pdf.setFillColorRGB(0, 0, 255) 
-#     pdf.setFont("Arial", 24) 
-#     pdf.drawCentredString(290, 720, subTitle) 
-#     pdf.line(30, 710, 550, 710) 
-#     text = pdf.beginText(40, 680) 
-#     text.setFont("Arial", 18) 
-#     for line in textLines: 
-#         text.textLine(line) 
-#     pdf.drawText(text) 
-#     pdf.drawInlineImage(image, 155, 200) #x.y
-#     pdf.save()
