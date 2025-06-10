68fd556122ae95ed871e6c945ca9189cec4dda84
dm283 <dm283@yandex.ru>
2025-06-10 17:45:48 +0400

    attach-from-ea done

--- frontend/src/components/FormEAList.vue
+++ frontend/src/components/FormEAList.vue
@@ -35,7 +35,6 @@ const authHeader = () => {
   if (user && user.access_token) {return { Authorization: 'Bearer ' + user.access_token };} else {return {};}
 }
 
-
 state.query = `http://${backendIpAddress}:${backendPort}/document_records/`
 state.listTableColumns = {
     'doc_name':'Наименование','doc_id':'Номер','doc_date':'Дата','contact_uuid':'Контрагент','created_datetime':'Дата загрузки'
@@ -43,38 +42,26 @@ state.listTableColumns = {
 state.additionalColumns = {  };
 state.listItemFileds = {...state.listTableColumns, ...state.additionalColumns};
 
-
 async function getData() {
     state.isLoading = true;
     try {     
       const response = await axios.get(state.query, {headers: authHeader()});
       state.records = response.data;
-    } catch (error) {
-      console.error('Error fetching', error);
-    } finally {
-      state.isLoading = false;
-    }
+    } catch (error) { console.error('Error fetching', error); } 
+    finally { state.isLoading = false; }
 }
 
 onMounted(async () => {
     await getData()
 });
 
-
-const chooseDoc = (item) => {
-  if (item) { state.choosenDocs.push(item) }
-}
-
-const returnToObjForm = () => {
-  // console.log('return with', state.choosenDocs.length, 'documents')
-  emit('returnedDocs', state.choosenDocs); emit('closeModal');
-}
+const chooseDoc = (item) => { if (item) { state.choosenDocs.push(item) } }
+const returnToObjForm = () => { emit('returnedDocs', state.choosenDocs); emit('closeModal'); }
 
 </script>
 
 <template>
-  <div class="w-74 max-h-4/5 bg-white drop-shadow-md rounded-lg overflow-hidden">
-  
+  <div class="w-3/5 bg-white drop-shadow-md rounded-lg overflow-auto">
     <header class="py-2 pl-6 bg-slate-200 text-black text-lg font-normal">
       Электронный архив
       <div class="absolute top-2 right-4 cursor-pointer hover:text-gray-500">
@@ -82,12 +69,12 @@ const returnToObjForm = () => {
       </div>
     </header>
 
-    <button class="formBtn" type="button" @click="returnToObjForm()">ПРИВЯЗАТЬ</button>
-
     <div class="flex">
-      <div class="" v-for="doc in state.choosenDocs">
-        <div class="">{{ doc.doc_name }}</div>
-      </div>
+    <button class="mt-3 ml-3 formBtn" type="button" @click="returnToObjForm()">ПРИВЯЗАТЬ</button>
+    <div v-for="doc in state.choosenDocs"
+      class="inline-block h-8 mt-3.5 ml-2 text-sm font-semibold text-white rounded-md px-2 py-1 bg-indigo-300" >
+      {{ doc.doc_name }}
+    </div>
     </div>
 
   <!-- Show loading spinner while loading is true -->
@@ -102,7 +89,7 @@ const returnToObjForm = () => {
     <div class="flex-auto w-auto md:w-64">
       <div class="">
         <ListAdv @btn-choose="chooseDoc"
-          :name="'Электронный архив'" :data="state.records" :listTableColumns="state.listTableColumns" :listItemFileds="state.listItemFileds"/>
+          :name="'Выбор документов'" :data="state.records" :listTableColumns="state.listTableColumns" :listItemFileds="state.listItemFileds"/>
       </div>
     </div>
   </div>
